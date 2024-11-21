19e8be0892c7390b17bed6e50863c79ed6bafcfd
Andrés Uribe <aeua2000@gmail.com>
2024-11-20 22:17:43 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="background">
+    <header className="header">
+      <div className="logo">
+        <img src="src/assets/servicesjmk_azul.png" alt="Services JMK Logo" />
+      </div>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <a href="/" className="nav-link">Home</a>
+          </li>
+          <li className="nav-item">
+            <a href="/about" className="nav-link">About Us</a>
+          </li>
+          <li className="nav-item">
+            <a href="/contact" className="nav-link">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+      <div className="linea-separadora"></div>
+      <main className="main-content">
+        <p>Something great is on the way</p>
+        <h1>COMING SOON</h1>
+          <div className="spacer"></div>
+      </main>
+      <footer>
+        <p className="footer-text">&copy; 2024 <strong>Services JMK LLC</strong>. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App
