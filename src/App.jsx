dbc8d981dd569cf57a54bcc4a51bf9b0a97d0480
Andrés Uribe <aeua2000@gmail.com>
2024-11-27 19:25:14 -0400

    Se corrigió error de redireccionamiento de paginas en el nav-bar

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ function App() {
       <nav className="nav">
         <ul className="nav-list">
           <li className="nav-item">
-            <a href="/" className="nav-link">Home</a>
+            <a href="#" className="nav-link">Home</a>
           </li>
           <li className="nav-item">
-            <a href="/about" className="nav-link">About Us</a>
+            <a href="#" className="nav-link">About Us</a>
           </li>
           <li className="nav-item">
-            <a href="/contact" className="nav-link">Contact</a>
+            <a href="#" className="nav-link">Contact</a>
           </li>
         </ul>
       </nav>
