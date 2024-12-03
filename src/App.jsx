d38f4f81f5d0dfbdcc5830105d321a1153c5fba2
Andrés Uribe <aeua2000@gmail.com>
2024-12-02 22:40:24 -0400

    update: actualizacion en de la interfaz

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,75 @@
+import {useState} from "react"
 import "./App.css"
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button} from "@nextui-org/react";
+import {AcmeLogo} from "./AcmeLogo.jsx";
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuItems = [
+    "Home",
+    "About",
+    "Contact",
+  ];
+
   return (
-    <div className="background">
-    <header className="header">
-      <div className="logo">
-        <img src="/servicesjmk.png"  alt="Services JMK Logo" />
-      </div>
-      <nav className="nav">
-        <ul className="nav-list">
-          <li className="nav-item">
-            <a href="#" className="nav-link">Home</a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-link">About Us</a>
-          </li>
-          <li className="nav-item">
-            <a href="# " className="nav-link">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-      <div className="linea-separadora"></div>
-      <main className="main-content">
-        <p>We're preparing for the next step</p>
-        <h1>COMING SOON</h1>
-          <div className="spacer"></div>
-      </main>
-      <footer>
-        <p className="footer-text">&copy; 2024 <strong>Services JMK LLC</strong>. All rights reserved.</p>
-      </footer>
-    </div>
+  <div className="background"> 
+    <Navbar shouldHideOnScroll onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <AcmeLogo />
+          <p className="font-bold text-inherit">Services JMK, LLC</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            About
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Contact
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={index === 0 ? "primary" : "foreground"} // Solo el primer ítem tiene el color primario
+              className={`w-full ${index === 0 ? "selected-item" : ""}`} // Agrega una clase si es el primer ítem
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+
+      
+    <main className="main-content">
+      <p className="text">We're preparing for the next step</p>
+      <h1>COMING SOON</h1>
+      <div className="spacer"></div>
+    </main>
+    <footer>
+      <p className="footer-text">&copy; 2024 <strong>Services JMK LLC</strong>. All rights reserved.</p>
+    </footer>
+  </div>
   );
 }
 
